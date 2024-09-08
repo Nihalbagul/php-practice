b67a048cc7fbffb18bbd6c46c1c58f15dcfa1138
Nihalbagul <nihalbagul98@gmail.com>
2024-09-08 18:43:02 +0530

    first commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,58 @@
+<?php
+// $a="welcome Nihal Bagul";
+// echo $a . "<br>";
+// $favcolor="red";
+// switch($favcolor){
+//     case "red":
+//         echo "your favorite color is red";
+//         break;
+//     case "blue":
+//         echo "your favorite color is blue";
+//         break;
+//     case "green":
+//         echo "your favorite color is green";
+//         break;
+//     default:
+//         echo "your favorite color is neither red, blue, nor green";
+// }
+
+// $x=10;
+// while($x<21){
+//     echo "The number is: $x <br>";
+//     $x++;
+// }
+
+// $x=1;
+// do{
+//     echo "The number is: $x <br>";
+//     $x++;
+// }while($x<=5)
+
+// for($i=0;$i<5;$i++){
+//     echo "The number is: $i <br>";
+// }
+
+// $name =array("Nihal","Bagul","Nihal","Bagul","Nihal","Bagul","Nihal","Bagul","Nihal","Bagul");
+// foreach($name as $value){
+//     echo $value . "<br>";
+// }
+
+// function hello($name){
+//     echo "hello $name <br>";
+// }
+// hello("Nihal");
+// hello("Bagul");
+
+// function sum($num1,$num2){
+//     echo $num1 + $num2 . "<br>";
+// }
+// sum(5,5);
+// sum(10,5);
+
+// array_merge();
+// array_push();
+// array_pop();
+
+
+
+?>
